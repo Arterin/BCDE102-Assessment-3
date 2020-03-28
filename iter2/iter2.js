2b8b7b2a4b2e729f346a463f1c1f951043e9fff1
Vivian Luxon <arterinv@gmail.com>
2020-03-28 16:58:38 +1300

    first commit

---
+++ iter2/iter2.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const papa = require('papaparse');
+
+const file = fs.createReadStream('partycsv.csv');
+
+class Party{
+    constructor(newPartyName, newVotes, newVotePercentage) {
+        this.partyName = newPartyName;
+        this.votes = newVotes;
+        this.votePercentage = newVotePercentage;
+    }
+}
+
+class Electorate{
+    constructor(newElectorateName) {
+        this.electorateName = newElectorateName;
+        this.partyCount = 0;
+        this.partyList = [];
+    }
+
+    addParty(newPartyName, newVotes, new_votes_percentage) {
+        let aParty = new Party(newPartyName, newVotes, new_votes_percentage);
+        this.partyCount += 1;
+        this.partyList.push(aParty);
+    }
+}
+
+anElectorate = new Electorate(newElectorateName="Kelston")
+
+papa.parse(file, {
+	complete: function(results) {
+        var parties = 0
+        parties = results.data;
+        //add all parties to anElectorate
+        parties.forEach(forEachAddToParty);
+        //stringify the JSON so we can save it to a file, otherwise it will look like "[object object]"
+        var anElectorateStr = JSON.stringify(anElectorate, null, 4);
+        console.log(anElectorateStr);
+
+        fs.writeFile('electorate.json', anElectorateStr, (err) => {
+            if (err) throw err;
+            console.log('file created!');
+        });
+        }
+});
+
+function forEachAddToParty(party) {
+    anElectorate.addParty(newPartyName=party[0], newVotes=party[1], newVotePercentage=party[02]);
+};
