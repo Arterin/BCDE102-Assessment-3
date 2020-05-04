e78cf244298d59e546bf348430cd042770269616
Vivian Luxon <arterinv@gmail.com>
2020-05-05 01:42:52 +1200

    Moved electorate.json to the correct directory. Deleted HelloWorld.vue.

--- iter3/src/App.vue
+++ iter3/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Electorate />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Electorate from './components/Electorate.vue'
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Electorate
   }
 };
 </script>
---
+++ iter3/src/components/Electorate.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>My Election Data App</h1>
+    <button v-on:click="getElectorateData">Get Electorate Data</button>
+    <div>{{electorateDataList}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Electorate",
+  data() {
+    return {
+      electorateDataList: []
+    };
+  },
+  methods: {
+    getElectorateData() {
+      fetch("electorate.json")
+        .then(response => response.json())
+        .then(data => (this.electorateDataList = data));
+    }
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
